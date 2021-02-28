44e30db6170fb1c486fa42e36177a043a935d476
wing <wongwingchit@gmail.com>
2021-02-28 11:55:47 +0800

    adding.travis and deploy

--- deploy.js
+++ deploy.js
@@ -2,7 +2,7 @@ var cmd = require('node-cmd');
 var path, node_ssh, ssh, fs;
 fs = require('fs');
 path = require('path');
-node_ssh = require('node-ssh');
+node_ssh = require('node-ssh@10.0.0');
 ssh = new node_ssh();
 
 // the method that starts the deployment process
